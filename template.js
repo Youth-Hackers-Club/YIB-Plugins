ad2b5b812866be34a3e1b8a0683ae4a3d5752f25
ScruffyRules <scott-scheiner@hotmail.com>
2014-06-05 22:11:55 +0930

    Test

--- template.js
+++ template.js
@@ -1,27 +1,26 @@
 var m;
-
 function getName() {//Must Match the filename (Including Case)
-    return "Template"
+	return "Template"
 }
 function getCommands() {//returns a "|" separated list of valid commands
-    return "TemplateCommand1|TemplateCommand2";
+	return "TemplateCommand1|TemplateCommand2";
 }
 function init(manager) {
-    m = manager//Stores the plugin manager for future use (Recommended)
+	m = manager//Stores the plugin manager for future use (Recommended)
 }
 function exec(command, args) {
-    if (command == "TemplateCommand1") {
-        var out = ""
-        out += "You have run TemplateCommand1";
-        return out;
-    } else if (command == "TemplateCommand2") {
-        var out = ""
-        out += "You have run TemplateCommand2";
-        return out;
-    } else {
-        return command;
-    }
+	if (command == "TemplateCommand1") {
+		var out = ""
+		out += "You have run TemplateCommand1";
+		return out;
+	} else if (command == "TemplateCommand2") {
+		var out = ""
+		out += "You have run TemplateCommand2";
+		return out;
+	} else {
+		return command;
+	}
 }
 function close(manager) {//Clean up anything if needed
-    println("Closing" + getName());
+	println("Closing" + getName());
 }
