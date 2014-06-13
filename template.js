470f3b433b74af50da6001b0dc115dbf3f9e6f41
scottramsay64 <scottramsay64@gmail.com>
2014-06-13 18:47:39 +0930

    Added event arg to exec methos in template.js

--- template.js
+++ template.js
@@ -1,24 +1,24 @@
 var m;
 function getName() {//Must Match the filename (Including Case)
-	return "Template"
+	return "Template";
 }
 function getCommands() {//returns a "|" separated list of valid commands
 	return "TemplateCommand1|TemplateCommand2";
 }
 function init(manager) {
-	m = manager//Stores the plugin manager for future use (Recommended)
+	m = manager;//Stores the plugin manager for future use (Recommended)
 }
-function exec(command, args) {
+function exec(command, event, args) {
 	if (command == "TemplateCommand1") {
-		var out = ""
+		var out = "";
 		out += "You have run TemplateCommand1";
 		return out;
 	} else if (command == "TemplateCommand2") {
-		var out = ""
+		var out = "";
 		out += "You have run TemplateCommand2";
 		return out;
 	} else {
-		return command;
+		return command + " Not found";//If this should never run
 	}
 }
 function close(manager) {//Clean up anything if needed
